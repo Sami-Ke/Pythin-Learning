db3a7261baedbac121d1bbb91101712f62af5de3
Sami-Ke <77624831+Sami-Ke@users.noreply.github.com>
2021-01-18 21:46:51 +0800

    Add files via upload

---
+++ D01作業 - 等差數列,取出偶數,3的倍數.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install numpy
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+np.__version__
+
+
+# In[ ]:
+
+
+#1.生成一個等差數列，首數為0，尾數為20，公差為1的數列。
+
+
+# In[60]:
+
+
+a=np.arange(21)
+
+
+# In[49]:
+
+
+print(a)
+
+
+# In[ ]:
+
+
+#2.呈上題，將以上數列取出偶數。
+
+
+# In[56]:
+
+
+a[2:21:2]
+
+
+# In[ ]:
+
+
+#3.呈1題，將數列取出3的倍數。
+
+
+# In[59]:
+
+
+a[1:20:2]
+
